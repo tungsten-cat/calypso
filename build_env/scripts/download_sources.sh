4c785fcb1dd99a3f0ff0964e572f915da48cb6bc
tungsten-cat <uly4nkin.denis@gmail.com>
2023-03-31 00:07:15 +0300

    Added README for building environment; Fixed several typos

--- build_env/scripts/download_sources.sh
+++ build_env/scripts/download_sources.sh
@@ -11,16 +11,16 @@ pushd ${downloading_path}
 
 # Using WGET we can download sources of binutils
 # With TAR will unpack archive containing sources
-wget https://ftp.gnu.org/gnu/binutils/binutils-${BINUTILS_VERSION}.tar.xz
-tar -xvf binutils-${BINUTILS_VERSION}.tar.xz
+wget -q https://ftp.gnu.org/gnu/binutils/binutils-${BINUTILS_VERSION}.tar.xz
+tar -xf binutils-${BINUTILS_VERSION}.tar.xz
 
 # The same for GCC sources
-wget https://ftp.gnu.org/gnu/gcc/gcc-${GCC_VERSION}/gcc-${GCC_VERSION}.tar.xz
-tar -xvf gcc-${BINUTILS_VERSION}.tar.xz
+wget -q https://ftp.gnu.org/gnu/gcc/gcc-${GCC_VERSION}/gcc-${GCC_VERSION}.tar.xz
+tar -xf gcc-${GCC_VERSION}.tar.xz
 
 # After unpacking sources we can remove downloaded files
 rm binutils-${BINUTILS_VERSION}.tar.xz
-rm gcc-${BINUTILS_VERSION}.tar.xz
+rm gcc-${GCC_VERSION}.tar.xz
 
 # POPD returns you to the latest directory stored on stack
 popd
