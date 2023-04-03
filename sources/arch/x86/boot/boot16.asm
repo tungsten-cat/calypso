8a4b19ec0e1e672367e967270a1081ce82f7987c
tungsten-cat <uly4nkin.denis@gmail.com>
2023-04-03 20:34:53 +0300

    Added code to enter Protected Mode

--- sources/arch/x86/boot/boot16.asm
+++ sources/arch/x86/boot/boot16.asm
@@ -3,6 +3,10 @@
     ; This is the point where booting process will start executing
     global _bootloader_entry
 
+    ; Also we should decalare _protected_mode_entry as extern label
+    ; As it defined in different file
+    extern _protected_mode_entry
+
     ; Our bootloader will be loaded by BIOS
     ; At the address 0x7C00, so we should tell NASM about it
     [org 0x7c00]
@@ -16,7 +20,25 @@
 ; After that we can create our start label
 ; Where our code will begin executing
 _bootloader_entry:
-    jmp _bootloader_entry
+    ; Firstly we should disable interrupts
+    ; Because it might break processing our code
+    cli
+    
+    ; Next we should load GDT
+    lgdt [GDT32_descriptor]
+
+    ; After that we can swtich to 32-bit mode 
+    ; By setting 32-bit mode in CR0 register
+    mov eax, cr0
+    or  eax, 0x1
+    mov cr0, eax
+
+    ; To change our CS register we should perform a far jump
+    jmp GDT32_start.GDT32_code:_protected_mode_entry
+
+
+; Also we should include file with our GDT32
+%include "gdt/gdt32.asm"
 
 ; Due the fact that boot sector should be 512 bytes long
 ; We're putting zeros to remaining bytes of it
---
+++ sources/arch/x86/boot/boot32.asm
@@ -0,0 +1,4 @@
+
+    ; We should declare our _protected_mode_entry label as global
+    ; That way it will be available anywhere in our project for executing
+    global _protected_mode_entry
