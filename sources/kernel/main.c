260e5512a81216426b280b10c85758cd2ca71582
tungsten-cat <uly4nkin.denis@gmail.com>
2023-05-24 23:03:14 +0300

    Another conflict fix

--- sources/kernel/main.c
+++ sources/kernel/main.c
@@ -3,13 +3,7 @@
     #error "You are not using a cross-compiler, this code may be compiled wrong!" 
 #endif
 
-<<<<<<< HEAD
 void kernel_entry(void)
 {
     
-=======
-void kernel_entry(void) 
-{
-	
->>>>>>> cfcf563177c2c1a39abc129536363fbab0674bfc
 }
